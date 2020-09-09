43e27e7f7fd238dabbd4ec32c9dad7a302dfdf77
Hee-jin506 <gmlwls35220@gmail.com>
2020-09-09 18:05:30 +0900

    회원 클래스 수정

--- src/main/java/mini/project/domain/Member.java
+++ src/main/java/mini/project/domain/Member.java
@@ -1,11 +1,113 @@
 package mini.project.domain;
 
+import mini.project.handler.MovieHandler;
+import mini.project.util.ArrayList;
+import mini.project.util.Iterator;
 import mini.project.util.List;
+import mini.project.util.Prompt;
 
 public class Member {
+  
+  private static final int FEMALE = 0;
+  private static final int MALE = 1;
+  
   String name;
   int age;
   int gender;
-  List<Movie> favoriteGenre;
+  Genre favoriteGenre;
   List<Movie> toWatchList;
+  
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public String getGender() {
+    switch (gender) {
+      case FEMALE:
+        return "여성";
+      default:
+        return "남성";
+    }
+  }
+  public void setGender(String gender) {
+    switch (gender) {
+      case "여성":
+        this.gender = FEMALE;
+      case "남성":
+        this.gender = MALE;
+    }
+  }
+  public String getFavoriteGenre() {
+    switch (member.getFavoriteGenre()) {
+      case HORROR:
+        return "Horror";
+      case ACTION:
+        return "Action";
+      case FAMILY:
+        return "Family";
+      case ROMANCE:
+        return "Romance";
+    }
+  }
+  public void setFavoriteGenre(String favoriteGenre) {
+    switch (favoriteGenre) {
+      case "Horror":
+        this.favoriteGenre = HORROR;
+      case "Action":
+        this.favoriteGenre = ACTION;
+      case "Family":
+        this.favoriteGenre = FAMILY;
+      case "Romance":
+        this.favoriteGenre = ROMANCE;
+    }
+  }
+  public String getToWatchList() {
+    StringBuilder toWatchList = new StringBuilder();
+    Iterator<Movie> movieIterator = this.toWatchList.iterator();
+    while (movieIterator.hasNext()) {
+      Movie movie = movieIterator.next();
+      if (toWatchList.length() > 0) {
+        toWatchList.append(",");
+      }
+      toWatchList.append(movie);
+    }
+    return toWatchList.toString(); 
+  }
+  
+//  public List<Movie> getToWatchList() {
+//    return toWtachList;
+//  }
+  
+//public String getToWatchList() {
+//  return toWatchList().toString();
+//}
+  
+  
+  
+  public void setToWatchList() {
+    List<Movie> toWatchList = new ArrayList<>();
+    // 영화 리스트 보여주는 화면 띄우기
+    while (true) {
+      String str = Prompt.inputString("찾으시는 영화의 제목을 입력하세요.(빈문자열: 넘어가기)");
+      if (str.length() == 0) {
+        break;
+      }
+      Movie movie = MovieHandler.findByTitle();
+      if (movie == null) {
+        System.out.println("찾으시는 영화가 없습니다. 다시 입력해주세요.");
+      } else {
+        toWatchList.add(movie);
+      }
+    }  
+    this.toWatchList = toWatchList;
+  }
+  
 }
--- src/main/java/mini/project/handler/MemberHandler.java
+++ src/main/java/mini/project/handler/MemberHandler.java
@@ -2,6 +2,7 @@ package mini.project.handler;
 
 import mini.project.domain.Member;
 import mini.project.util.ArrayList;
+import mini.project.util.Iterator;
 import mini.project.util.List;
 import mini.project.util.Prompt;
 
@@ -10,7 +11,7 @@ public class MemberHandler {
 
   public void manage() {
 
-    switch (Prompt.inputString("회원 추가\n회원 삭제\n 회원 수정\n회원 조회\n입력>(빈문자열:취소) ")) {
+    switch (Prompt.inputString("회원 추가\n회원 삭제\n회원 수정\n회원 조회\n입력>(빈문자열:취소) ")) {
       case "회원 추가":
         add();
         break;
@@ -25,21 +26,96 @@ public class MemberHandler {
         break;
     }
   }
-
+//
+//  String name;
+//  int age;
+//  int gender;
+//  List<Movie> favoriteGenre;
+//  List<Movie> toWatchList;
+//  
   public void add() {
+    System.out.println("[회원 등록]");
 
+    Member member = new Member();
+    member.setAge(Prompt.inputInt("나이? "));
+    while (true) {
+      String gender = Prompt.inputString("성별? ");
+      if (gender.equals("여성") || gender.equals("남성")) {
+        member.setGender(gender);
+        break;
+      } else {
+        System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+      }
+    }
+    member.setToWatchList();
+    memberList.add(member);
   }
 
   public void list() {
+    System.out.println("[회원 목록]");
 
-
+    Iterator<Member> iterator = memberList.iterator();
+      while (iterator.hasNext()) {
+        Member member = iterator.next();
+        System.out.printf("%d, %s, %s, %s, %s\n",
+          member.getAge(),
+          member.getGender(),
+          member.getFavoriteGenre(),
+          member.getToWatchList()
+          );
+    }
   }
 
   public void update() {
+    System.out.println("[회원 변경]");
+    String name = Prompt.inputString("이름? ");
+    Member member = findByName(name);
 
+    if (member == null) {
+      System.out.println("해당 번호의 회원이 없습니다.");
+      return;
+    }
+
+    int age = Prompt.inputInt(
+        String.format("나이(%d)? ", member.getName()));
+    String gender = Prompt.inputString(
+        String.format("성별(%s)? ", member.getGender()));
+    String favoriteGenre = Prompt.inputString(
+        String.format("취향 장르(%s)? ", member.getFavoriteGenre()));
+    // toWatchList 변경어떻게 할겨
+    
+    String response = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+    if (!response.equalsIgnoreCase("y")) {
+      System.out.println("회원 변경을 취소하였습니다.");
+      return;
+    }
+
+    member.setAge(age);
+    member.setGender(gender);
+    member.setFavoriteGenre(favoriteGenre);
+    // toWatchList 어떻게 할겨
+    
+    System.out.println("회원 변경하였습니다.");
   }
 
   public void delete() {
+    System.out.println("[회원 삭제]");
+    String name = Prompt.inputString("이름? ");
+    int index = indexOf(name);
+
+    if (index == -1) {
+      System.out.println("해당 이름의 회원이 없습니다.");
+      return;
+    }
+
+    String response = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
+    if (!response.equalsIgnoreCase("y")) {
+      System.out.println("회원 삭제를 취소하였습니다.");
+      return;
+    }
+
+    memberList.remove(index);
+    System.out.println("회원을 삭제하였습니다.");
 
   }
 
@@ -48,10 +124,22 @@ public class MemberHandler {
   }
 
   Member findByName(String name) {
+    for (int i = 0; i < memberList.size(); i++) {
+      Member member = memberList.get(i);
+      if (member.getName() == name) {
+        return member;
+      }
+    }
     return null;
   }
 
   int indexOf(String name) {
+    for (int i = 0; i < memberList.size(); i++) {
+      Member member = memberList.get(i);
+      if (member.getName() == name) {
+        return i;
+      }
+    }
     return -1;
   }
 
