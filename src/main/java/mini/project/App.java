2918ed14f48272d66c368f2d4151fb7fe8a9df63
Hee-jin506 <gmlwls35220@gmail.com>
2020-09-09 09:55:25 +0900

    commit

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -9,7 +9,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        System.out.println("최희진!");
+      System.out.println("안녕~");
     }
 }
