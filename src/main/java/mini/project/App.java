346786b68e03c3625abc7e5e23ef2ada8bac153a
Hee-jin506 <gmlwls35220@gmail.com>
2020-09-10 12:13:09 +0900

    기본 영화 추가

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -3,7 +3,7 @@
  */
 package mini.project;
 
-import mini.project.domain.Genre;
+import mini.project.basic_movie.Movies;
 import mini.project.domain.Member;
 import mini.project.domain.Movie;
 import mini.project.handler.MemberHandler;
@@ -15,48 +15,17 @@ import mini.project.util.Screen;
 
 
 public class App {
-  // Screen.BeforeSignUpScreen()
-  // 회원가입 권유
-  // Screen.AfterSignUpScereen()
-  // 메뉴처리(MemberHandler, MovieHandler)
-
-
 
   public static void main(String[] args) throws InterruptedException {
+    
     List<Movie> movieList = new ArrayList<>();
+    Movies.addBasicMovies(movieList);
     MovieHandler movieHandler = new MovieHandler(movieList);
     List<Member> memberList = new ArrayList<>();
     MemberHandler memberHandler = new MemberHandler(memberList, movieHandler);
 
     // 로그인한 사용자
     Member loggedInMember = null;
-
-
-
-    for (int i = 0; i < 20; i++) {
-      Movie movie;
-      if (i < 5) {
-        movie = new Movie(String.format("%d", i), Genre.로맨스);
-        movie.setViewCount(1);
-      } else if (i < 10) {
-        movie = new Movie(String.format("%d", i), Genre.액션);
-        movie.setViewCount(2);
-      } else if (i < 15) {
-        movie = new Movie(String.format("%d", i), Genre.호러);
-        movie.setViewCount(3);
-      } else {
-        movie = new Movie(String.format("%d", i), Genre.가족);
-        movie.setViewCount(4);
-      }
-      movieList.add(movie);
-    }
-
-
-
-    /*
-     * if (Prompt.inputString("회원가입하시겠습니까?(y/N)").equalsIgnoreCase("y")) { loggedInMember =
-     * memberHandler.add(); } else { System.out.println("프로그램을 종료합니다."); return; }
-     */
 
     loop: while (true) {
 
